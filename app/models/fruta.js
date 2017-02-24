3bdf720229849f7b002a68842734d7ca78270220
Thiago <thiago.nogueira187@gmail.com>
2017-02-24 12:38:25 -0300

    Modelo do banco de dados

---
+++ app/models/fruta.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+
+module.exports = function(){
+  var fruteiraDb = mongoose.Schema({
+    nome:{
+      type: String,
+      required: true,
+      index:{
+        unique: true
+      }
+    },
+    quantidade:{
+      type: Number,
+      required: true
+    },
+    preco:{
+      type: Number,
+      required: true
+    }
+  });
+  
+  return mongoose.model("fruteira", fruteiraDb);
+}

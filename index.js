73ca16ed74ecc3d6402f810487f37f48ef9d714d
thiago <thiago.nogueira187@gmail>
2018-03-22 14:30:08 -0300

    [FIX] Corrigindo front end

---
+++ index.js
@@ -0,0 +1,5 @@
+var app = require('./config/express.js')()
+
+app.listen(app.get('port'), function () {
+  console.log('Servidor executando na porta ' + app.get('port'))
+})
--- node-server.js
+++
@@ -1,5 +0,0 @@
-var app = require("./config/express.js")();
-
-app.listen(app.get('port'), function(){
-  console.log('Servidor executando na porta ' + app.get('port'));
-});
